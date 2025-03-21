c8df8e172f98eadbe078fe2c2b706e7003fc6fdd
pranavacchu <pranavacharya360@gmail.com>
2025-03-21 23:23:50 +0530

    2nd commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World, i am pranav\n";
+  cout << "Hello, World, i am pranav from PES UNIVERSITY\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
